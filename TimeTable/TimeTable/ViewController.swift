bcdd6b3effaca2d99def85da985e177e172064b7
KazutomoKita <elliottstar77@yahoo.co.jp>
2020-03-02 19:02:37 +0900

    プロジェクト作成

---
+++ TimeTable/TimeTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeTable
+//
+//  Created by Kazutomo Kita on 2020/03/02.
+//  Copyright © 2020 Kazutomo Kita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
